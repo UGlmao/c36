fafdb06e21c203fe7eb1b5838ec583776a9a741e
UGlmao <91505825+UGlmao@users.noreply.github.com>
2022-01-04 20:03:54 -0600

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,25 @@
+class Form{
+  constructor(){
+
+  }
+  display(){
+      var title = createElement('h2');
+      title.html("Juego de carreras");
+      title.position(200,50);
+      var input = createInput("name");
+      var button = createButton('play');
+      input.position(130,160);
+      button.position(250,200);
+      button.mousePressed(function(){
+          input.hide();
+          button.hide();
+          var name = input.value();
+          playerCount+= 1;
+          player.update(name);
+          player.updateCount(playerCount);
+          var greeting = createElement('h3');
+          greeting.html("Hola "+ name);
+          greeting.position(130,160);
+      });
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var gameState = 0;
+var playerCount;
+var database;
+var form,game,player;
+
+function setup(){
+  var canvas = createCanvas(400,400);
+  database = firebase.database();
+  
+  game = new Game();
+  
+  game.getState();
+  game.start();  
+}
+
+function draw(){
+  bg("white");
+}
